a4661205251a72278d9671b044d8b690a5afb7b2
Ainz <127408603+Jashk120@users.noreply.github.com>
2026-04-28 13:48:29 +0530

    docs: update components/ABC.tsx

--- components/ABC.tsx
+++ components/ABC.tsx
@@ -1,8 +1,17 @@
+```typescript
 import React from "react";
 
+/**
+ * ABC component.
+ * 
+ * A simple React functional component that displays the text "HI".
+ * 
+ * @returns A React JSX fragment containing the text "HI".
+ */
 export default function ABC (){
    
    
    return<>
     HI</>
 }
+```
