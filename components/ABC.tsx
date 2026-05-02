bd271136d2e0a859fd65c77284dc547ebeaaef17
Ainz <127408603+Jashk120@users.noreply.github.com>
2026-05-02 18:14:28 +0530

    docs: update components/ABC.tsx

--- components/ABC.tsx
+++ components/ABC.tsx
@@ -1,8 +1,15 @@
+```typescript
 import React from "react";
 
+/**
+ * A simple React component that renders the text "HI".
+ *
+ * @returns A JSX element displaying "HI".
+ */
 export default function ABC (){
    
    
    return<>
     HI</>
 }
+```
